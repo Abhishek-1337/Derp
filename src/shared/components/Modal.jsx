bbfa59a631c3ee4e15ddab88bce646dc2f037985
Abhishek-1337 <abhishekrinku4@gmail.com>
2024-03-11 01:50:13 +0530

    Fixed UI bug

--- src/shared/components/Modal.jsx
+++ src/shared/components/Modal.jsx
@@ -17,13 +17,13 @@ const Modal = ({ isOpen, onCancel, data }) => {
     <div
       className={`${
         isOpen ? "" : "hidden"
-      } absolute top-0 right-0 left-0 bottom-0 h-screen w-[100vw] z-20`}
+      } fixed top-0 right-0 left-0 bottom-0  w-[100vw] z-20`}
     >
       <div
         className="absolute bg-blue-300 opacity-40 top-0 w-full h-screen z-10"
         onClick={onCancel}
       ></div>
-      <div className="absolute top-0 right-0 bottom-0 left-0 max-w-[500px] max-h-max bg-white m-auto z-30 p-4 rounded-xl pt-6 pb-6">
+      <div className="absolute top-0 right-0 bottom-0 left-0 max-w-[500px] max-h-max bg-white m-auto z-30 p-4 rounded-xl pt-6">
         <div className="flex items-center justify-center">{data}</div>
       </div>
     </div>,
