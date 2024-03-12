9deafefe5f1058bf634728415ec6e5d993801646
Abhishek-1337 <abhishekrinku4@gmail.com>
2024-03-12 20:30:47 +0530

    Dynamic window in Modal

--- src/shared/components/Modal.jsx
+++ src/shared/components/Modal.jsx
@@ -1,7 +1,7 @@
 import { createPortal } from "react-dom";
 import { useEffect } from "react";
 
-const Modal = ({ isOpen, onCancel, data, title }) => {
+const Modal = ({ isOpen, onCancel, data, title, additionalStyles }) => {
   useEffect(() => {
     if (isOpen) {
       document.body.style.overflowY = "hidden";
@@ -25,11 +25,15 @@ const Modal = ({ isOpen, onCancel, data, title }) => {
         className="absolute bg-blue-300 opacity-40 top-0 w-full h-screen z-10"
         onClick={onCancel}
       ></div>
-      <div className="absolute top-0 right-0 bottom-0 left-0 max-w-[500px] max-h-max  m-auto z-30  p-4">
+      <div
+        className={`absolute top-0 right-0 bottom-0 left-0 max-w-[500px] max-h-max  m-auto z-30  p-4 ${additionalStyles} `}
+      >
         <header className="bg-gray-500 rounded-t-2xl p-2 text-center text-white font-medium ">
           {title}
         </header>
-        <section className="flex items-center justify-center bg-white p-4 overflow-x-auto">
+        <section
+          className={`flex items-center justify-center bg-white p-4 overflow-x-auto `}
+        >
           {data}
         </section>
         <footer className="bg-gray-500 rounded-b-2xl p-2 text-center text-white font-medium  shadow-lg shadow-gray-300"></footer>
