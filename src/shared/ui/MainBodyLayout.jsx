b76242b9011cebcad23c68801329184e1747f61b
Abhishek-1337 <abhishekrinku4@gmail.com>
2024-03-15 04:01:35 +0530

    fix: UI enhancement

--- src/shared/ui/MainBodyLayout.jsx
+++ src/shared/ui/MainBodyLayout.jsx
@@ -1,6 +1,6 @@
 const MainBodyLayout = (props) => {
   return (
-    <div className="md:w-[calc(100%-250px)] md:relative z-[0] md:left-[250px] p-4 pt-5 md:pt-20 min-h-max ">
+    <div className="md:w-[calc(100%-250px)] md:relative z-[0] md:left-[250px] p-4 pt-4 md:pt-[4rem] min-h-max ">
       {props.children}
     </div>
   );
